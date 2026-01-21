a4cf1b7ff5af1e026f5a037742571d2199db439a
68010126 <68010126@kmitl.ac.th>
2026-01-21 14:16:27 +0700

    add

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -11,19 +11,8 @@ if (
 ) {
   console.log("Invalid Input");
 }
-
-if (score < 0){
-    console.log("Invalid input")
-}else if(score > 0 && score <= 49){
-    console.log("F")
-}else if(score > 49 && score <= 59){
-    console.log("D")
-}else if(score > 59 && score <= 69){
-    console.log("C")
-}else if(score >69 && score <= 79){
-    console.log("B")
-}else if(score > 79 && score <= 100){
-    console.log("A")
-}else{
-    console.log("Invalid input")
-}
+if (score >= 80) console.log("A");
+else if (score >= 70) console.log("B");
+else if (score >= 60) console.log("C");
+else if (score >= 50) console.log("D");
+else console.log("F");
