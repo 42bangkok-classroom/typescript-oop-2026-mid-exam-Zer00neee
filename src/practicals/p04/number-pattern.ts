fdae913de3099abaed4563f013d3445cf7fbb751
68010126 <68010126@kmitl.ac.th>
2026-01-21 15:21:28 +0700

    add

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -2,17 +2,17 @@ const args = process.argv.slice(2);
 const size = parseInt(args[0]);
 
 if (!isNaN(size) && size > 0) {
-
-    for (let i = 1; i <= size; i++) {
-        
-      
-    
-        const spaces = " ".repeat(size - i);
    
-        const row = Array.from({ length: i }, (_, j) => {
-            return i - j;
-        }).join("");
+    const maxRow = Array.from({ length: size }, (_, j) => size - j).join("");
+    const maxWidth = maxRow.length;
+    
+ 
+    for (let i = 1; i <= size; i++) {
+     
+        const row = Array.from({ length: i }, (_, j) => i - j).join("");
         
+       
+        const spaces = " ".repeat(maxWidth - row.length);
         
         console.log(spaces + row);
     }
