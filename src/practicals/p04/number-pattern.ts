18504a2a3476ecf880ad885e714ddc21ac6d9b32
68010126 <68010126@kmitl.ac.th>
2026-01-21 14:57:30 +0700

    add

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,18 +1,19 @@
-// Write your code below
 const args = process.argv.slice(2);
 const size = parseInt(args[0]);
 
 if (!isNaN(size) && size > 0) {
-    for (let i = 0; i < size; i--) {
+
+    for (let i = 0; i < size; i++) {
         
         const count = size - i;
-   
+        
+      
         const row = Array.from({ length: count }, (_, j) => {
-         
-            return count - j;
+      
+            return j + 1;
         }).join("");
         
-   
+      
         console.log(row);
     }
 }
