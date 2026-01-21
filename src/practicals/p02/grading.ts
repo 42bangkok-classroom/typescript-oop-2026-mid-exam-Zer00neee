5fc50305a44fe826c966548bd0a9642155208c3b
68010126 <68010126@kmitl.ac.th>
2026-01-21 14:35:42 +0700

    add

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -3,18 +3,19 @@ const arg = process.argv[2];
 const score = Number(arg);
 
 
-if (
-  arg === undefined ||
-  Number.isNaN(score) ||
-  score < 0 ||
-  score > 100
-) {
-  console.log("Invalid Input");
-  break;
-}
 
-else if (score >= 80) console.log("Grade is A");
-else if (score >= 70) console.log("Grade is B");
-else if (score >= 60) console.log("Grade is C");
-else if (score >= 50) console.log("Grade is D");
-else console.log("Grade is F");
+if (score < 0){
+    console.log("Invalid input")
+}else if(score > 0 && score <= 49){
+    console.log("Grade is F")
+}else if(score > 49 && score <= 59){
+    console.log("Grade is D")
+}else if(score > 59 && score <= 69){
+    console.log("Grade is C")
+}else if(score >69 && score <= 79){
+    console.log("Grade is B")
+}else if(score > 79 && score <= 100){
+    console.log("Grade is A")
+}else{
+    console.log("Invalid input")
+}
